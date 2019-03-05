304a84fd77e4ca50af837fdae0e7c077f422e35e
Pratima <pratima@new-host-2.local>
2019-03-05 19:11:18 +0100

    using refs to  access dom elements

--- src/App.js
+++ src/App.js
@@ -5,24 +5,10 @@ import './App.css';
 
 class App extends Component { 
 
-  constructor(props) {
-    super(props);
-    console.log('constructor');
-  }
-//this fired before the render 
-  componentWillMount() {
-    console.log('will mount');
-  }
-  componentDidMount () {
-    console.log('mounted');
-  }
-   state = {
-     toggle: true
-   } 
-   toggle = () => {
-     this.setState({
-       toggle: !this.state.toggle
-     })
+   submit = () => {
+     console.log(this.text.value );
+     console.log(this.email.value );
+     
    }
 
 
@@ -31,22 +17,19 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text ="introduction to props" toggle = {this.state.toggle}/>
+          <Welcome text ="introduction to props" />
           
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >
+            ref={(input) => this.text = input}>
             Learn React
           </a>
-
-          {this.state.toggle &&  
-          <p>This should show and hide.</p>
-        }         
-     
-        <button onClick={this.toggle}> show/hide</button>
+<input type="text" ref={(input) => this.text = input} />
+<input type="email" ref={(input) => this.email = input} />
+        <button onClick={this.submit}> show value</button>
         </header>
 
         
@@ -58,9 +41,8 @@ class App extends Component {
 class Welcome extends Component {
   render() {
     // do some js before return
-    const { text, toggle} = this.props; 
-    console.log(toggle);
-    return (
+    const { text } = this.props; 
+      return (
       <h1 className="App-title">{ text }</h1>
       // passing data from parent class to child class
       //<h1 className="App-title">{this.props.text}</h1>
